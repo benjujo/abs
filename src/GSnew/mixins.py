a2d15fe2abdaa22dc1b2203379e2a1c53534fb1f
Benjamín Hurtado <git@benjamin.hurta.do>
2024-10-29 13:41:38 -0300

    mixins

---
+++ src/GSnew/mixins.py
@@ -0,0 +1,18 @@
+
+
+
+
+class GrothSahai:
+    def __init__(self):
+        self.variables = []
+        self.equations = []
+        
+    
+    def gsfy(self):
+        raise NotImplementedError("Method not implemented")
+    
+    def add_variable(self):
+        raise NotImplementedError("Method not implemented")
+    
+    def add_equation(self):
+        raise NotImplementedError("Method not implemented")
