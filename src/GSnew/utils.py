d481bd0d2fdbb0dd8ac2e97176bc7269301d83ad
Benjamín Hurtado <git@benjamin.hurta.do>
2024-11-19 12:02:26 -0300

    partial47

--- src/GSnew/utils.py
+++ src/GSnew/utils.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+
+def extended_pair(a, b):
+    # Check that a and b have dimension 2
+    if a.shape != (2,) or b.shape != (2,):
+        raise ValueError(f"Invalid dimensions: a.shape={a.shape}, b.shape={b.shape}")
+    return np.array( [a[0].pair(b[0]), a[1].pair(b[0]), a[0].pair(b[1]), a[1].pair(b[1])] )
+
+
+def pair(a, b):
+    return a.pair(b)
+
 class CustomArray(np.ndarray):
     def __new__(cls, input_array):
         obj = np.asarray(input_array).view(cls)
@@ -19,3 +30,57 @@ class CustomArray(np.ndarray):
                 result[i, j] = sum(func(self[i, k], other[k, j]) for k in range(self.shape[1]))
         
         return result
+    
+    def e(self, other):
+        func = pair
+        return self.matfunc(other, func)
+    
+    def E(self, other):
+        func = extended_pair
+        return self.matfunc(other, func)
+    
+    
+class NamedArray(np.ndarray):
+    def __new__(cls, input_data):
+        # Extract the data, names, and element types from the input tuples
+        names = [item[0] for item in input_data]
+        data = [item[1] for item in input_data]
+        
+        # Create the ndarray
+        obj = np.asarray(data).view(cls)
+        # Store the names and element types as instance attributes
+        obj.names = names
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None: return
+        self.names = getattr(obj, 'names', None)
+
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            index = self.names.index(key)
+            return super().__getitem__(index)
+        else:
+            return super().__getitem__(key)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, str):
+            index = self.names.index(key)
+            super().__setitem__(index, value)
+        else:
+            super().__setitem__(key, value)
+            
+    def __json__(self):
+        # Create a list of dicts with 'name' and 'value'
+        return {name: value for name, value in zip(self.names, self.tolist())}
+
+    # Optional: Define a custom JSON encoder to handle NamedArray objects
+    class NamedArrayEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, NamedArray):
+                return obj.__json__()
+            if isinstance(obj, Element):
+                return obj.__json__()
+            if isinstance(obj, np.ndarray):
+                return obj.tolist()
+            return super().default(obj)
